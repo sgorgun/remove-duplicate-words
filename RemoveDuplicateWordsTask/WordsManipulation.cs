5b03d981bdc65acac88c87d824409f020216c2ac
sgorgun <sergei.gorgun@gmail.com>
2023-11-08 16:32:24 +0200

    Ready

--- RemoveDuplicateWordsTask/WordsManipulation.cs
+++ RemoveDuplicateWordsTask/WordsManipulation.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Text;
 
 namespace RemoveDuplicateWordsTask
 {
+    /// <summary>
+    /// Remove duplicate words.
+    /// </summary>
     public static class WordsManipulation
     {
         /// <summary>
@@ -15,7 +19,45 @@ namespace RemoveDuplicateWordsTask
         /// <exception cref="ArgumentException">If source text is null or empty.</exception>
         public static void RemoveDuplicateWords(ref string? text, bool caseSensitive)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentException("Source text is null or empty");
+            }
+
+            StringBuilder wordFromText = new StringBuilder();
+            List<string> words = new List<string>();
+            HashSet<string> wordsWitoutDuplicates = new HashSet<string>(caseSensitive ? StringComparer.Ordinal : StringComparer.OrdinalIgnoreCase);
+            StringBuilder result = new StringBuilder();
+
+            foreach (var symbol in text)
+            {
+                if (char.IsLetterOrDigit(symbol))
+                {
+                    wordFromText.Append(symbol);
+                }
+                else
+                {
+                    if (wordFromText.Length > 0)
+                    {
+                        words.Add(wordFromText.ToString());
+                        wordFromText.Clear();
+                    }
+
+                    words.Add(symbol.ToString());
+                }
+            }
+
+            if (wordFromText.Length > 0)
+            {
+                words.Add(wordFromText.ToString());
+            }
+
+            foreach (var word in words)
+            {
+                result.Append(wordsWitoutDuplicates.Add(word) || !char.IsLetterOrDigit(word[0]) ? word : string.Empty);
+            }
+
+            text = result.ToString();
         }
     }
 }
